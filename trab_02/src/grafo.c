47b53899c0865a857a414fc59aa187296884bbe1
Ricardo Norio Miyata <ricardomyt98@gmail.com>
2019-05-23 18:19:59 -0300

    First implementation of functions.

--- trab_02/src/grafo.c
+++ trab_02/src/grafo.c
@@ -6,7 +6,6 @@
 
 #define VERTICE_SIZE 10
 #define VERTICE_NEIGHBOR_SIZE 50
-#define MAX_STRING_LENGTH 1024
 #define MAX_LINE_LENGTH 2048
 
 // Vertice structure
@@ -28,7 +27,16 @@ typedef struct _grafo
 } * grafo;
 
 // Function struct _verticeto destroy the graph
-int destroi_grafo(grafo g);
+int destroi_grafo(grafo g)
+{
+    for (int i = 0; i < g->numVertices; i++)
+    {
+        free(g->vertices[i]);
+    }
+    free(g);
+
+    return 1;
+}
 
 // Function to initialize a vertice struct
 vertice create_vertice(char *name)
@@ -55,11 +63,12 @@ grafo create_graph()
     return g;
 }
 
+// Function to search for a vertice or create and insert a new one in the graph.
 vertice search_or_create_vertice(grafo g, char *name)
 {
     for (int i = 0; i < g->numVertices; i++)
     {
-        if (g->vertices[i]->name == name)
+        if (strcmp(g->vertices[i]->name, name) == 0)
         {
             return g->vertices[i];
         }
@@ -78,6 +87,7 @@ vertice search_or_create_vertice(grafo g, char *name)
     return newVertice;
 }
 
+// Function to check if a given vertice is or not neighbor of another.
 bool is_neighbor(vertice target, vertice neighbor)
 {
     for (int i = 0; i < target->numNeighbors; i++)
@@ -91,6 +101,7 @@ bool is_neighbor(vertice target, vertice neighbor)
     return false;
 }
 
+// Function to insert a vertice in the neighbors of another.
 void add_neighbor(vertice target, vertice neighbor)
 {
     if (!is_neighbor(target, neighbor))
@@ -105,6 +116,7 @@ void add_neighbor(vertice target, vertice neighbor)
     }
 }
 
+// Function to make a bidirectional link between two vertices.
 void link_vertices(grafo g, char *a, char *b)
 {
     vertice newA, newB;
@@ -116,6 +128,7 @@ void link_vertices(grafo g, char *a, char *b)
     add_neighbor(newB, newA);
 }
 
+// Function to check if a letter exists in a given string.
 bool have_letter(char *line)
 {
     while (*line)
@@ -130,23 +143,23 @@ bool have_letter(char *line)
     return false;
 }
 
+// Function to read a file and return a graph.
 grafo le_grafo(FILE *input)
 {
-    char line[MAX_STRING_LENGTH];
-    grafo g = create_graph();
-
     if (input)
     {
-        while ((fgets(line, MAX_LINE_LENGTH, input)) != NULL)
+        char line[MAX_LINE_LENGTH];
+        grafo g = create_graph();
+
+        while (fgets(line, sizeof(line), input))
         {
             if (have_letter(line))
             {
                 int i = 0;
-                char *token, *newline; // The "token" store the return of splitted string
-                char *array[2] = {NULL, NULL};
+                char *token, *newline, *array[2] = {NULL}; // The "token" store the return of splitted string.
                 const char delim[2] = " ";
 
-                // Handle the "\ n" of the line to not store the vertex name with it
+                // Handle the "\n" of the line to not store in the vertex name.
                 newline = strchr(line, '\n');
                 if (newline)
                 {
@@ -155,45 +168,127 @@ grafo le_grafo(FILE *input)
 
                 token = strtok(line, delim);
 
-                while (token != NULL)
+                while (token != NULL) // Split a string and store it into an array with two positions (two vertices at most)
                 {
                     array[i++] = token;
                     token = strtok(NULL, delim);
                 }
 
-                if (array[0] != NULL && array[1] != NULL)
+                if (array[0] != NULL && array[1] != NULL) // Two vertices
                 {
-                    link_vertices(g, array[0], array[1]);
+                    char *vStr1 = (char *)malloc(strlen(array[0]) * sizeof(char));
+                    char *vStr2 = (char *)malloc(strlen(array[1]) * sizeof(char));
+
+                    strcpy(vStr1, array[0]);
+                    strcpy(vStr2, array[1]);
+
+                    link_vertices(g, vStr1, vStr2);
                 }
-                else
+                else // One vertice
                 {
-                    search_or_create_vertice(g, array[0]);
+                    char *vStr1 = (char *)malloc(strlen(array[0]) * sizeof(char));
+
+                    strcpy(vStr1, array[0]);
+
+                    search_or_create_vertice(g, vStr1);
                 }
             }
         }
+
+        fclose(input);
+
+        return g;
     }
     else
     {
         perror("Error to read the input file.\n");
         exit(1);
     }
+}
+
+// Recursive function to go through for all the vertices and neighbors and put these in a file
+void visit_graph_vertices(FILE *output, vertice v)
+{
+    v->processed = true;
+
+    for (int i = 0; i < v->numNeighbors; i++)
+    {
+        if (v->neighbors[i]->processed == false)
+        {
+            fprintf(output, "%s %s\n\n", v->name, v->neighbors[i]->name);
+        }
+    }
+
+    for (int i = 0; i < v->numNeighbors; i++)
+    {
+        if (v->neighbors[i]->processed == false)
+        {
+            visit_graph_vertices(output, v->neighbors[i]);
+        }
+    }
+}
+
+// Function to set the "processed" variable to false in entire graph
+void set_vertice_processed_flag(grafo g)
+{
+    if (g->numVertices == 0)
+    {
+        printf("The graph is empty.\n");
+    }
+    else
+    {
+        for (int i = 0; i < g->numVertices; i++)
+        {
+            g->vertices[i]->processed = false;
+        }
+    }
+}
+
+// Function to print a graph in stdout
+grafo escreve_grafo(FILE *output, grafo g)
+{
+    visit_graph_vertices(output, g->vertices[0]);
+    set_vertice_processed_flag(g);
 
     return g;
 }
 
-grafo escreve_grafo(FILE *output, grafo g);
+// Returns the number of vertices of a graph.
+unsigned int n_vertices(grafo g)
+{
+    return g->numVertices;
+}
 
-unsigned int n_vertices(grafo g);
-
+// Returns the number of edges of a graph.
 unsigned int n_arestas(grafo g);
 
-vertice vertice_nome(grafo g, char *nome);
+// Returns a vertice searched for in a graph by a given string.
+vertice vertice_nome(grafo g, char *nome)
+{
+    for (int i = 0; i < g->numVertices; i++)
+    {
+        if (strcmp(g->vertices[i]->name, nome) == 0)
+        {
+            return g->vertices[i];
+        }
+    }
 
-char *nome(vertice v);
+    return NULL;
+}
+
+// Returns the name of a vertice.
+char *nome(vertice v)
+{
+    return v->name;
+}
 
 unsigned int cobertura_por_trilhas(grafo g, vertice **cobertura[]);
 
+// The main function.
 int main()
 {
+    FILE *output = fopen("output.txt", "w");
     grafo g = le_grafo(stdin);
+    escreve_grafo(output, g);
+    destroi_grafo(g);
 }
